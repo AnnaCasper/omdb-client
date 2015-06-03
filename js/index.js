f4984e994afd4f67e8b13b0550cb7aaf7fa6d650
Anna Casper <anna.casper3@gmail.com>
2015-06-03 11:00:39 -0600

    index.html works

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
-console.log("hello");
+var h1 = document.createElement('h1');
+h1.innerHTML = "OMBD Movies About Pizza";
+document.body.appendChild(h1);
+
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'http://www.omdbapi.com/?s=pizza');
+xhr.addEventListener('load', function(){
+  var response = JSON.parse(xhr.responseText);
+
+  for (var i = 0; i < response.Search.length; i++) {
+
+
+    var a = document.createElement('a');
+    a.innerHTML = response.Search[i].Title;
+    var url = "/show.html?" + "i=" + response.Search[i].imdbID;
+    a.href = url
+    var p = document.createElement('p');
+    p.appendChild(a);
+    document.body.appendChild(p);
+  };
+});
+xhr.send(null);
--- js/show.js
+++ js/show.js
@@ -1,1 +1,22 @@
-console.log("!");
+queryString.parse(url);
+
+var req = new XMLHttpRequest;
+var imdbArray = [];
+function findImdbId() {
+  for (var i = 0; i < response.Search.length; i++) {
+  imdbArray[i] = response.Search[i].imdbID
+  }
+  return imdbArray
+};
+
+
+var imdbId = imdbArray[i];
+var url = "http://www.omdbapi.com/?i=" + parseInt(imdbId);
+req.open('GET', url)
+req.addEventListener('load', function(){
+  var response = JSON.parse(req.responseText);
+  var h1 = document.createElement('h1');
+  h1.innerHTML = response.Title
+  document.appendChild('h1');
+});
+req.send(null);
